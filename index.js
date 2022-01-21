5d8da65ed006bcbf6e0acf36117d4cac647f4a5f
Will <willchesson@gmail.com>
2022-01-21 12:26:05 -0500

    commit tree d73e49cba0724af0bbf86690f3811f6eae793f09

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(sentence => {
+    const words = sentence.split(" ")
+    return words.map((word) => {
+      return word[0].toUpperCase() + word.substring(1);
+    }).join(" ");
+  })
 }
+
+
+
